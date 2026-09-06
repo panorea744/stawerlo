a19aec08c387cda8538a442f20f68e9f8484ea84
panorea744 <titanevpan1@gmail.com>
2026-09-06 22:11:18 +0300

    Update global.py

--- global.py
+++ global.py
@@ -28,7 +28,7 @@ STATIC_LOGO = "https://i.hizliresim.com/8xzjgqv.jpg"
 # ============================================
 
 SELCUK_NAMES = {
-    "sbeinsports-1": "beIN Sports 1", "selcukobs1": "beIN Sports 1", "selcukbeinsports1": "beIN Sports 1",
+    "sbeinsports-1": "beIN Sports 1", "selcukobs1": "beIN Sports 1 (Alternatif 2)", "selcukbeinsports1": "beIN Sports 1 (Alternatif 3)",
     "selcukbeinsports2": "beIN Sports 2", "selcukbeinsports3": "beIN Sports 3",
     "selcukbeinsports4": "beIN Sports 4", "selcukbeinsports5": "beIN Sports 5",
     "selcukbeinsportsmax1": "beIN Sports Max 1", "selcukbeinsportsmax2": "beIN Sports Max 2",
