bb9acef07d00fd1d684486a115893ed4dbd06263
panorea744 <titanevpan1@gmail.com>
2026-09-06 16:21:50 +0300

    Update zeus_bot.py

--- zeus_bot.py
+++ zeus_bot.py
@@ -3,8 +3,8 @@ import re
 import os
 
 # --- KONFIGÜRASYON ---
-BASE_DOMAIN_PATTERN = "zeustv{}.vip"
-START_INDEX = 262
+BASE_DOMAIN_PATTERN = "zeustv{}.cfd"
+START_INDEX = 269
 END_INDEX = 500
 REQUEST_TIMEOUT = 5  
 GITHUB_FOLDER_NAME = "teyzeniyerim1"
